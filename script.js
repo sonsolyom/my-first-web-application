f71155e86e9873cdfdc1f1d6c000aa8b996ab8bb
sonsolyom <nagydsolyom@gmail.com>
2020-05-07 11:17:24 +0200

    additional css
    made the last part of css, with the thumbnail titles.
    and in script made new thumbanil pictures.

--- script.js
+++ script.js
@@ -6,41 +6,49 @@ let photoNumber = 0;
 let imagesData = [
     {
         photo: './img/Notorius Bounty Hunters_resize.jpg',
+        thumbnail: './img/thumbnail/Notorius Bounty Hunters_tn.jpg',
         title: 'Notorius Bounty Hunters',
         descripion: 'The most feared warriors and hunters of the galaxy!'
     },
     {
         photo: './img/Clones of the Empire_rezise.jpg',
+        thumbnail: './img/thumbnail/Clones of the Empire_tn.jpg',
         title: 'Clones of the Empire',
         descripion: 'Created to protect and defend the Republic.'
     },
     {
         photo: './img/Utility Droids.jpg',
+        thumbnail: './img/thumbnail/Utility Droids_tn.jpg',
         title: 'Utility Droids',
         descripion: 'If you would like to go somewhere through space, you will need one.'
     },
     {
         photo: './img/TIE Fighter.jpg',
+        thumbnail: './img/thumbnail/TIE Fighter_tn.jpg',
         title: 'TIE Fighter',
         descripion: 'When you hear one, you will already know... trouble is ahead!'
     },
     {
         photo: './img/X-wing.jpg',
+        thumbnail: './img/thumbnail/X-wing_tn.jpg',
         title: 'X-wing',
         descripion: 'Functional and effective - one of it have dealt with the Death Star 1.'
     },
     {
         photo: './img/Weaopon of the Jedii.jpg',
+        thumbnail: './img/thumbnail/Weaopon of the Jedii_tn.jpg',
         title: 'Weaopon of the Jedii',
         descripion: 'Use it with care, it is far more than a weapon!'
     },
     {
         photo: './img/Lord Vader_resize.jpg',
+        thumbnail: './img/thumbnail/Lord Vader_tn.jpg',
         title: 'Lord Vader',
         descripion: 'Fear and power incarnate!'
     },
     {
         photo: './img/Millenium Falcon.jpg',
+        thumbnail: './img/thumbnail/Millenium Falcon_tn.jpg',
         title: 'Millenium Falcon',
         descripion: 'The fastest frigatte in the galaxy!'
     },
@@ -103,7 +111,8 @@ imagesData.forEach( (element, index) => {
     $('.all-thumbnail-container').append(
         `
         <div class="small-thumbnail-container" data-number=${index}>
-            <img id="" src="${element.photo}" alt="${element.title}" data-number=${index}>
+            <img id="" src="${element.thumbnail}" alt="${element.title}" data-number=${index}>
+            <div class="thumbnail-title">${element.title}</div>
         </div>
         `
     );
